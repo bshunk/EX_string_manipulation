f9ccc7782f4824abd6b28b223c667b6a82ef629e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-28 15:44:28 -0500

    work on string manip JS

--- strings.js
+++ strings.js
@@ -9,10 +9,28 @@ console.log("ahoy mate")
 // 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
 // The output of each of the functions should immediately appear as well.
 
-function reversal() {
-	document.getElementById("userInput").
+
+// console.log("Numbers = ", [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8]
+// 	.sort(function(a, b) {
+// 		return b - a
+// 	}).filter( function(filtered) {
+// 		return filtered < 19
+// 	}).map(function(mapped) {
+// 		return mapped * 1.5 - 1
+// 	}).reduce(function(x, y) {
+// 		return x + y
+// 	}));
+
+
+function reversal(userInput) {
+	var userInput = document.getElementById("userInput");
+	userInput.sort(function(userInput) {
+		return b - a
+	})
 }
 
+
+//
 function alphabits() {
 
 }
