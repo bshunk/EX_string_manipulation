4860aceb15aaf7310f21f8875a3c042b6d8acc6f
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-05 12:49:42 -0500

    work on getting numbers not to be accepted as arguments in the input

--- strings.js
+++ strings.js
@@ -1,15 +1,19 @@
 // 1. Copy the code below into the JavaScript file
 // 2. In your HTML, create an text input and a button.
 // 3. The text input should only accept letters. No numbers.
-// 4. Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
-// 5. Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
-// 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
-// 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
+// 4. Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the 
+// result in the DOM, below the text input.
+// 5. Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the 
+// result in the DOM, below the text input.
+// 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display 
+// the text "Your string is a palidrome" in the DOM, below the text input.
+// 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable 
+// (see below) to the value of the input.
 // The output of each of the functions should immediately appear as well.
 
-var string = document.getElementById("input");
-var button = document.getElementById("button").addEventListener("click", doTheThing);
-var gimmeIt = document.getElementById("box");
+// var string = document.getElementById("input");
+// var button = document.getElementById("button").addEventListener("click", doTheThing);
+// var gimmeIt = document.getElementById("box");
 
 function doTheThing() {
 	var newString = string.value.split("");
@@ -38,7 +42,14 @@ function palindrome(str, revStr) {
 
 // FUNCTIONS RETURN TO WHERE IT IS CALLED
 
-// var testString = "";
+var testString = document.getElementById("input"); 
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
+
+
+
+
 
 // function reversal(testString) {
 // 	gimmeIt.innerHTML += `<p>${string.value.split("").reverse().join("")}</p>`; 
