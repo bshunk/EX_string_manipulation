953ed0854bc161dca779b9491e7b863cab74f918
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 11:55:06 -0500

    pair program with david and help get a better understanding of what JS needs to do in order to function correctly

--- strings.js
+++ strings.js
@@ -1,15 +1,12 @@
-// 1. Copy the code below into the JavaScript file
-// 2. In your HTML, create an text input and a button.
-// 3. The text input should only accept letters. No numbers.
-// 4. Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
-// 5. Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
-// 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
-// 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
-// The output of each of the functions should immediately appear as well.
 
 let string = document.getElementById("input");
 let button = document.getElementById("button");
 let gimmeIt = document.getElementById("box");
+let button1 = 
+var testString = "";
+// adds a keyup event listener for the enter button and if true it runs the letterOnly fuction
+
+
 
 button.addEventListener("click", doTheThing);
 
@@ -27,8 +24,7 @@ function reversal(str) {
 }
 
 function alphabits(str) {
-	var alpha = str.sort().join("");
-	return alpha;
+	return str.sort().join("");;
 };
 
 function palindrome(str, revStr) {
@@ -39,20 +35,16 @@ function palindrome(str, revStr) {
 	}
 }
 
-// FUNCTIONS RETURN TO WHERE IT IS CALLED
-// var testString = "";
+function letterOnly() {
+	// make a function that grabs the element for the text string, puts it into a variablr, creates a condition stating that if the input is NOT a letter- it's alerts that only letters are allowed and clears it, yet if it is a letter ir uns the doTheThing functions  
+	// 
+	// if (!letter.match(/[a-zA-z]/))
+}
 
-// function reversal(testString) {
-// 	gimmeIt.innerHTML += `<p>${string.value.split("").reverse().join("")}</p>`; 
-// 	return string.value.split("").reverse().join("")
-// };
+function clearInput() {
+	testString = "";
+	output.innerHTML = "";
+}
 
-// function alphabits(testString) {
-// 	gimmeIt.innerHTML += `<p>${string.value.split("").sort().join("")}</p>`;
-// };
 
-// function palindrome(testString) {
-// 	if (string.value.split("").reverse().join("") === string.value) {
-// 		return `<p>This is a palindrome</p>`
-// 	}
-// };
+
