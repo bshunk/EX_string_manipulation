4ba6d8dc25a3cdb15de5f39a312ccd78f7bad582
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 11:13:53 -0500

    get the three needed functions to work in order to manipulate the strings

--- strings.js
+++ strings.js
@@ -4,100 +4,51 @@
 // 4. Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
 // 5. Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
 // 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
-// 7. When the user p1resses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
+// 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
 // The output of each of the functions should immediately appear as well.
 
-let input = document.getElementById("input");
+let string = document.getElementById("input");
+let button = document.getElementById("button");
+let gimmeIt = document.getElementById("box");
 
+button.addEventListener("click", doTheThing);
 
+function doTheThing() {
+	let newString = string.value.split("");
+	let revString = reversal(newString);
+	gimmeIt.innerHTML += alphabits(newString);
+	gimmeIt.innerHTML += palindrome(newString, revString);
+};
 
-function reversal() {
-
+function reversal(str) {
+	var rev = str.reverse().join("");
+	gimmeIt.innerHTML += `<p>${rev}</p>`; 
+	return rev
 }
 
-function alphabits() {
+function alphabits(str) {
+	var alpha = str.sort().join("");
+	return alpha;
+};
 
+function palindrome(str, revStr) {
+	if (revStr === string.value) {
+		return `<p>This is a palindrome</p>`
+	} else {
+		return `<p>This is not a palindrome</p>`
+	}
 }
-
-function palindrome() {
-
-}
-
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var string = document.getElementById("input");
-// var button = document.getElementById("button").addEventListener("click", doTheThing);
-// var gimmeIt = document.getElementById("box");
-
-// function doTheThing() {
-// 	var newString = string.value.split("");
-// 	var revString = reversal(newString);
-// 	gimmeIt.innerHTML += alphabits(newString);
-// 	gimmeIt.innerHTML += palindrome(newString, revString);
-// };
-
-// function reversal(str) {
-// 	var rev = str.reverse().join("");
-// 	gimmeIt.innerHTML += `<p>${rev}</p>`; 
-// 	return rev
-// }
-
-// function alphabits(str) {
-// 	return `<p>${str.sort().join("")}</p>`;
-// }
-
-// function palindrome(str, revStr) {
-// 	if (revStr === string.value) {
-// 		return `<p>This is a palindrome</p>`
-// 	} else {
-// 		return `<p>This is not a palindrome</p>`
-// 	}
-// }
 
 // FUNCTIONS RETURN TO WHERE IT IS CALLED
-
 // var testString = "";
 
 // function reversal(testString) {
 // 	gimmeIt.innerHTML += `<p>${string.value.split("").reverse().join("")}</p>`; 
-// 	console.log("reversal", gimmeIt.innerHTML);
 // 	return string.value.split("").reverse().join("")
 // };
 
 // function alphabits(testString) {
 // 	gimmeIt.innerHTML += `<p>${string.value.split("").sort().join("")}</p>`;
-// 	console.log("alphabits ", gimmeIt.innerHTML);
 // };
 
 // function palindrome(testString) {
@@ -105,14 +56,3 @@ palindrome(testString);
 // 		return `<p>This is a palindrome</p>`
 // 	}
 // };
-
-
-
-
-
-
-
-
-
-
-
